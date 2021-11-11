ba011722609ef1c9cfdb08b3b3b8562db68ed256
aaron-truong <47168113+aaron-truong@users.noreply.github.com>
2021-11-11 15:53:45 -0800

    Create seg_tree_iterative.cpp

---
+++ range_queries/seg_tree_iterative.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, arr[100001];
+
+template<class T> struct Seg {
+    const T INIT=0; 
+    int n; vector<T> seg;
+    T FUNC(T a, T b) {return a+b;} 
+    void init(int _n) {
+        n=_n;
+        seg.assign(2*n, INIT);
+    }
+	void init(int _n, T _a[]) {
+        init(_n);
+        for(int i=n; i<2*n; i++) seg[i]=_a[i-n];
+        for(int i=n-1; i>=1; i--) seg[i]=FUNC(seg[i<<1], seg[i<<1|1]);
+    }
+    void update(int i, T val) {
+        for(seg[i+=n]=val; i>1; i>>=1) 
+            seg[i>>1]=FUNC(seg[i], seg[i^1]);
+    }
+    T query(int l, int r) {
+        T ret=INIT;
+        for(l+=n, r+=n; l<r; l>>=1, r>>=1) {
+            if(l&1) ret=FUNC(ret, seg[l++]);
+            if(r&1) ret=FUNC(ret, seg[--r]);
+        }
+        return ret;
+    }
+};
